dd14986c6c1cf0dbdeff0c9d5f4c434410746797
Oleg Chukhlantsev <Nerevar5@mail.ru>
2023-12-30 10:31:13 +0300

    Review changes

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -3,7 +3,11 @@ public class Cat implements Pet{
     @Override
     public void Sound()
     {
-        System.out.println("Meow-meow");
+        System.out.println("meow-meow");
     }
 
+    public void makeAnAction()
+    {
+        System.out.println("Cat caught a mouse");
+    }
 }
