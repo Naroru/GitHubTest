e47ac6ade1a8ceaf2cbe117b550dd8ca00a279a4
naro <Nerevar5@mail.ru>
2024-10-14 11:41:12 +0300

    cat not run

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -12,7 +12,7 @@ public class Cat implements Pet{
     }
     public void run()
     {
-        System.out.println("Cat is running");
+        System.out.println("Cat is not running");
     }
        public void sleeping()
     {
